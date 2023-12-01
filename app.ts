71723c88d92baedaf769da29628cac405422cbbb
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-01 10:17:20 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,10 +2,22 @@
 interface Pessoa {
     nome:String,
     idade:number,
-
+    profissao?:String // "?" Atributo é OPCIONAL !!!!!
 }
 
-const pessoa: Pessoa = {
+const pessoa:Pessoa = {
     nome: "Gabriel",
     idade: 24
 }
+
+const outraPessoa: Pessoa = {
+    nome: "Isa",
+    idade: 23,
+    profissao: "MEDVET"
+}
+
+// Array de objetos Pessoa
+const pessoas: Pessoa[] = [
+    pessoa,
+    outraPessoa
+];
