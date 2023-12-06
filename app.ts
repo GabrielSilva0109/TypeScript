6b34beb4da4ad51e49743e7552afd3545119f4e2
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-06 11:53:37 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
-const arrayNum: Array<number> = [1,2,3];
-const arrayNum2: number[] = [1, 2,3]
-
+const arrayNum: Array<number> = [1,2,3,5,6,7]
 
 const arrayString: string[] = ["a" , "b", "c"]
 
+console.log(arrayNum.length)
