63f0f5383b8a2822bc3b5e681839b0b8567bc914
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-06 13:00:07 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,4 +2,8 @@ const arrayNum: Array<number> = [1,2,3,5,6,7]
 
 const arrayString: string[] = ["a" , "b", "c"]
 
-console.log(arrayNum.length)
+console.log("Array origianl " + arrayNum)
+
+arrayNum.push(7)
+
+console.log("Array novo " + arrayNum)
