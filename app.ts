dee8bea0363198f0833da0ee2b8178edbf1b31f9
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-12 10:44:52 -0300

    1

--- app.ts
+++ app.ts
@@ -1,64 +1,12 @@
-//Banco
-//name | accountNumber
-//DEPOSITAR | SACAR
-
-//Classe de Conta Bancaria
-abstract class Account {
-    name: String
-    accountNumber: number
-    balance: number = 0
-
-    constructor(name: String, accountNumber: number){
-        this.name = name
-        this.accountNumber = accountNumber
-    }
-
-    deposit = () =>{
-        console.log("Voçê Depositou!")
-    }
-    
-    withraw = () =>{
-        console.log("Voçê Sacou!")
-    }
-
-    getBalance = () => {
-        console.log(this.balance)
-    }
-}
-
-//Classe de Administrador
-class Admin extends Account{
-
-    constructor(name:string, accountNumber: number){
-        super(name, accountNumber)
-        this.balance = 10
-    }
-
-    
-}
-
-class PeopleAccount extends Account{
-    doc_id:number
-
-    constructor(doc_id:number, name:String, accountNumber:number){
-        super(name, accountNumber)
-        this.doc_id = doc_id
-    }
-}
-
-class CompanyAccount extends Account {
-    constructor(name: string, accountNumber: number){
-        super(name, accountNumber)
-    }
-
-    getLoan = () =>{
-        console.log("Voce pegou um emprestimo")
-    }
-
-}
+import { CompanyAccount } from './class/CompanyAccount'
+import { DioAccount } from './class/DioAccount'
+import { PeopleAccount } from "./class/PeopleAccount"
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, "Gabriel", 10)
 console.log(peopleAccount)
 
 const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20)
-console.log(companyAccount)
+companyAccount.deposit()
+
+peopleAccount.setName("Gabriel Silva")
+console.log(peopleAccount.getName())
---
+++ class/DioAccount.ts
@@ -0,0 +1,51 @@
+//Classe de Conta Bancaria
+export abstract class DioAccount {
+    private name: String
+    accountNumber: number
+    balance: number = 0
+    status: boolean = true
+
+    constructor(name: String, accountNumber: number){
+        this.name = name
+        this.accountNumber = accountNumber
+    }
+
+    getName = (): String =>{
+        return this.name
+    }
+
+    setName = (name: String): void  => {
+        this.name = name
+        console.log("Nome Alterado !!")
+    }
+
+    deposit = (): void =>{
+        if(this.validadeStatus()){
+            console.log("Voçê Depositou!")
+        }
+    }
+    
+    withraw = (): void  =>{
+        console.log("Voçê Sacou!")
+    }
+
+    getBalance = (): void => {
+        console.log(this.balance)
+    }
+
+    private validadeStatus = () => {
+        if(this.status){
+            return this.status
+        }
+
+        throw new Error()
+    }
+}
+
+//Classe de Administrador
+export class Admin extends DioAccount{
+    constructor(name:string, accountNumber: number){
+        super(name, accountNumber)
+        this.balance = 10
+    }
+}
