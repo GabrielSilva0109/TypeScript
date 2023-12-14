804d47e104cc61e503baffa0ad259396396a0943
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-14 11:59:22 -0300

    1

--- my-app-ts/src/service/soma.test.tsx
+++ my-app-ts/src/service/soma.test.tsx
@@ -1,3 +1,13 @@
+import { multiplica, soma } from './soma'
+
 describe('soma', () => {
-    it('Deve somar 1 ao numero informado')
+    it('Deve somar 1 ao numero informado', () => {
+        const value = soma(1)
+        expect(value).toBe(2)
+    })
+
+    it("Deve multiplicar o numero por dois" , () => {
+        const value = multiplica(2)
+        expect(value).toBe(4)
+    })
 })
