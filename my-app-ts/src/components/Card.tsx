eab6f892a2413f5c5cc6d0557383798f1c2810ca
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-18 12:25:03 -0300

    Update Card.tsx

--- my-app-ts/src/components/Card.tsx
+++ my-app-ts/src/components/Card.tsx
@@ -35,14 +35,12 @@ export const Card = () => {
     console.log(userData)
 
     return (
-      <Box minHeight="100vh" background="#351547" padding="25px">
+      <Box minHeight="100vh" background="#359" padding="25px">
         <Box background="#fff" borderRadius="25px" padding="15px">
           <Center>
             <h1>Faça o Login</h1>
           </Center>
-          <Center>
-            
-          </Center>
+          <Center></Center>
           <Input placeholder='Email' marginBottom={'5px'} value={email} onChange={(event) => setEmail(event.target.value)}/>
           <Input placeholder='Password'/>
           <Center>
