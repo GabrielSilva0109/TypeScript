88c906f61f8e0e674982d0d6460b61899dfd7dbf
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-01 10:09:38 -0300

    Update Header.tsx

--- my-app-ts/src/components/Header/Header.tsx
+++ my-app-ts/src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import { Button, Center, Box, Text } from '@chakra-ui/react'
 import './Header.css'
 
 export const Header = () => {
     return(
-        <div>Dio Bank</div>
+        <Box background='orange' width='100%'>
+            <Center>
+                <Text fontSize='3x1'>Dio Bank</Text>
+            </Center>
+            <Button>
+                Sair
+            </Button>
+        </Box>
     )
 }
